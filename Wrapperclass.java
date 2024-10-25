b0b29ee1e90577ff439964336d898f561a31aa9c
shalinipaidimuddala <shalinipaidimuddala@gmail.com>
2024-10-25 12:53:07 +0530

    Add files via upload

---
+++ Wrapperclass.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+
+    // create primitive types
+    int a = 5;
+    double b = 5.65;
+
+    //converts into wrapper objects
+    Integer aObj = Integer.valueOf(a);
+    Double bObj = Double.valueOf(b);
+
+    if(aObj instanceof Integer) {
+      System.out.println("An object of Integer is created.");
+    }
+
+    if(bObj instanceof Double) {
+      System.out.println("An object of Double is created.");
+    }
+  }
+}
+//Primitive Types to Wrapper Objects
