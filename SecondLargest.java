d1e9c69cbe88e0f31d57fd8bbb1327e92ff7fbab
Paidimuddala Shalini <shalinipaidimuddala@gmail.com>
2024-10-29 11:57:07 +0530

    Add files via upload

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,25 +1,22 @@
-public class SecondLargest{
-public static void main(String[]args){
-int[]arr={5,1,9,7,3};
-if(arr.length<2){
-System.out.println("Array needs at least two elements"?);
-return ;
-}
-int firstLargest=Integer.MIN_VALUE;
-int secondLargest=Interger.MIN_VALUE;
-for(int num:arr){
-if(num>firstLargest){
-secondLargest=firstLargest;
-firstLargest=num;
-}else if(num>secondLargest&& num!= firstLargest){
-secondLargst=num;
-}
-}
-if(secondLargest==Integer.MIN_VALUE){
-System.out.println("There is no second largest element");
-}else{
-System.out.println("Second largest element is:
-"+secondLargest);
-}
-}
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 4, 45, 99};
+        int secondLargest = findSecondLargest(arr);
+        System.out.println("The second largest element is: " + secondLargest);
+    }
+
+    public static int findSecondLargest(int[] arr) {
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > first) {
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                second = num;
+            }
+        }
+        return second;
+    }
 }
