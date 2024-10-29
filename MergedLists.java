4440d19e3bc3534fda05afbb3199d43c891ff4e6
Paidimuddala Shalini <shalinipaidimuddala@gmail.com>
2024-10-29 15:30:29 +0530

    Add files via upload

---
+++ MergedLists.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MergeLists{
+public static void main(String[]args){
+List<Integer>list1=Arrays.asList(1,2,3);
+List<Integer>list2=Arrays.asList(3,4,5);
+Set<Integer>meregdList=new ArrayList<>(mergedSet);
+System.out.println(mergedList);//output:[1,2,3,4,5]
+}
+}
